2773fc8cddd86cadd6815afa158007198ef57382
yamanidev <elamine.yamani@gmail.com>
2026-06-12 10:28:40 +0100

    feat: add Linux and macOS specific ZSH configuration

---
+++ linux/.config/zsh/os.zsh
@@ -0,0 +1,1 @@
+# Linux-specific zsh config. Deployed to $ZDOTDIR/os.zsh and sourced by .zshrc.
---
+++ macos/.config/zsh/os.zsh
@@ -0,0 +1,8 @@
+# macOS-specific zsh config. Deployed to $ZDOTDIR/os.zsh and sourced by .zshrc.
+
+# Homebrew (Apple Silicon prefix; use /usr/local on Intel Macs).
+if [[ -x /opt/homebrew/bin/brew ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+elif [[ -x /usr/local/bin/brew ]]; then
+  eval "$(/usr/local/bin/brew shellenv)"
+fi
