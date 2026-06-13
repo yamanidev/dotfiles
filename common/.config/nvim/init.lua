75fff559d6472b90ea040547f9e73414b3a7f8d1
yamanidev <elamine.yamani@gmail.com>
2026-06-13 09:22:15 +0100

    chore: add more nvim configuration

--- common/.config/nvim/init.lua
+++ common/.config/nvim/init.lua
@@ -1,1 +1,8 @@
 vim.o.relativenumber = true
+
+-- Search: case-insensitive unless the query contains a capital letter.
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Prompt to save instead of erroring when quitting a file with unsaved changes.
+vim.o.confirm = true
